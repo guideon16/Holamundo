7f22563d132608c2891f8bfefca58d860bbf15c1
guideon16 <55553676+guideon16@users.noreply.github.com>
2019-09-19 20:18:50 +0200

    Create holamundo.java

---
+++ java/holamundo.java
@@ -0,0 +1,15 @@
+public class HolaEjemMundo {//clase principal para bebes
+  public static void main (String[] args) {
+      System.out.println("AL DUQUE DE BÉJAR, MARQUÉS DE GIBRALEÓN, Conde de Benalcázar y Bañares, Vizconde de la Puebla de Alcocer, 
+      Señor de las villas de Capilla, Curiel y Burguillos EN fe1 del buen acogimiento y honra que 
+      hace vuestra Excelencia a toda suerte de libros, como Príncipe tan inclinado a favorecer las buenas artes, 
+      mayormente las que por su nobleza no se abaten2 al servicio y granjerías del vulgo,3 he determinado de sacar a 
+      luz4 al Ingenioso hidalgo don Quijote de la Mancha al abrigo del clarísimo nombre de vuestra Excelencia, a quien, 
+      con el acatamiento que debo a tanta grandeza, suplico le reciba agradablemente en su protección, para que a su sombra, 
+      aunque desnudo de aquel precioso ornamento de elegancia y erudición de que suelen andar vestidas las obras que se componen 
+      en las casas de los hombres que saben, ose parecer seguramente en el juicio5 de algunos que, no continiéndose6 en los límites 
+      de su ignorancia, suelen condenar con más rigor y menos justicia los trabajos ajenos;7 
+      que, poniendo8 los ojos la prudencia de vuestra Excelencia en mi buen deseo, fío9 que 
+      no desdeñará la cortedad de tan humilde servicio.)
+      }
+      }
